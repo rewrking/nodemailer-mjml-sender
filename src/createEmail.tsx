0ac04b979eaeb863c1754d88c91ac6c6833cef44
Andrew King <pepsiotaku@gmail.com>
2019-05-29 09:59:52 -0400

    ts-node fix

--- src/createEmail.tsx
+++ src/createEmail.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { render } from "mjml-react";
 
-import CatFact, { CatFactProps } from "./layouts/CatFact";
-
 import mjmlOptions from "./mjmlOptions";
+import CatFact, { CatFactProps } from "./layouts/CatFact";
 
 export default (props: CatFactProps) => {
     const result = render(<CatFact fact={props.fact} />, mjmlOptions);
